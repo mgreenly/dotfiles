23b10ae632884df50b9146ba8cf2d0ae57d0a645
Michael Greenly <mgreenly@gmail.com>
2020-07-12 11:38:38 -0500

    drop the scripts folder

--- scripts/README.md
+++
@@ -1,3 +0,0 @@
-# Scripts
-
-Stuff that has not yet, or may never be, rolled into the normal bootstrap
--- scripts/nvidia-fix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export CC=/usr/bin/gcc-4.6
-./home/mgreenly/Downloads/NVIDIA-Linux-x86_64-340.46.run
