0fe43e6f72e772f702c7b4b51b50127a5083ae4a
Michael Greenly <mgreenly@gmail.com>
2025-04-12 12:59:16 -0500

    make apply dir save

--- apply.sh
+++ apply.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+pushd $PWD
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 for d in */ ; do
     echo "$d"
     stow --verbose --target $HOME --dir $PWD "$d"
 done
+
+popd
